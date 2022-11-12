cdfd9029201a600d00682270040e8384acc480dd
LizRanZim <lrzclass22@gmail.com>
2022-11-12 13:59:54 -0500

    Update readme and package.json

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -4,18 +4,43 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
 
-    // *** start Liz added code - this is from 19-28 base code
-        // Store the triggered events
-        window.deferredPrompt = event;
+// *** start Liz added code - this is from 19-28 base code
+    // Store the triggered events
+    window.deferredPrompt = event;
 
-        // Remove the hidden class from the button.
-        butInstall.classList.toggle('hidden', false);
-          // *** end Liz added code - this is from 19-28 base code
+    // Remove the hidden class from the button.
+    butInstall.classList.toggle('hidden', false);
+// *** end Liz added code - this is from 19-28 base code
 
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+// *** start Liz added code - this is from 19-28 base code
+
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+        return;
+    }
+
+    // shows the prompt
+    promptEvent.prompt();
+
+    // resets the deferred prompt variable
+    window.deferredPrompt = null;
+
+    butInstall.classList.toggle('hidden', true);
+
+
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+
+    // clears prompt
+    window.deferredPrompt = null;
+
+});
+
+// *** end Liz added code - this is from 19-28 base code
---
+++ README.md
@@ -0,0 +1,30 @@
+# Just Another Text Editor (J.A.T.E.)
+
+The name of the application is Just Another Text Editor.
+
+The application is a simple text editor that can be run from the browser or downloaded as a standalone app from the browser.
+
+## Mockup
+
+The following .gifs shows the application's appearance and functionality.
+
+Application running in Chrome Browser.
+
+<img src=".\Assets\RuninChrome.GIF" width ="700"> 
+
+Service Worker in Chrome Browser.
+
+<img src=".\Assets\ServiceWorker.GIF" width ="700">  
+
+Application running from local install.
+
+<img src=".\Assets\Local.GIF" width ="700"> 
+
+
+## Notes
+This app is deployed on Heroku.
+
+## Link to GitHub code repository for application
+
+<a href="https://github.com/LizRanZim/TextEditor">
+https://github.com/LizRanZim/TextEditor</a>
